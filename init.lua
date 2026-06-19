98e19c1ba25ff7ae4010d98dd340a6db4627450b
Daniel Gongora <danielgong@zillowgroup.com>
2026-06-19 13:36:33 -0600

    nvim-configs

--- init.lua
+++ init.lua
@@ -16,10 +16,30 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\" 
 
+-- Settings
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
 require("lazy").setup({
 	spec = { 
 		{import = "plugins"},
 	},
 	checker = { enabled = true },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local opts = { buffer = args.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end,
 })
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,26 +1,66 @@
 -- ~/.config/nvim/lua/plugins.lua
 return {
-  {
-    "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood",
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",   -- stable name for lazy.nvim
-    priority = 1000,       -- load before other plugins
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-	transparent_background = true,
-	auto_integrations = true,
-	dim_inactive = {
-	   enabled = true,
-	   shade = "dark",
-	   percentage = 0.70,
-	},
-      })
-      vim.cmd.colorscheme "catppuccin-nvim"
-    end,
-  },
+    -- Theme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",   -- stable name for lazy.nvim
+        priority = 1000,       -- load before other plugins
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha",
+	            transparent_background = true,
+            	auto_integrations = true,
+                dim_inactive = {
+                   enabled = true,
+                   shade = "dark",
+                   percentage = 0.70,
+                },
+            })
+            vim.cmd.colorscheme "catppuccin-nvim"
+        end,
+    },
+    -- Practice
+    {
+        "ThePrimeagen/vim-be-good",
+        cmd = "VimBeGood",
+    },
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim"
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+            { "<leader>fd", "<cmd>Telescope live_grep<cr>" },
+            { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+            { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+        },
+    },
+    -- Treesitter
+    {
+        "nvim-treesitter/nvim-treesitter"
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter").setup({
+                ensure_installed = { "c", "javascript", "typescript", "kotlin", "lua", "markdown", "bash", "java", "scala" },
+                highlight = { enable = true },
+            })
+        end,
+    },
+    -- LSP
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.enable("kotlin_lsp")
+        end,
+    },
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({ options = { theme = "auto" } })
+        end,
+    },
 }
 
